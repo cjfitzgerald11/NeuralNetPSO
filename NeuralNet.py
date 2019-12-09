96466e66e678e9318399a3c0fda75dcea8ecb8a2
Connor FitzGerald <cjfitzgerald11@gmail.com>
2019-12-09 12:06:33 -0500

    added main class

---
+++ NeuralNet.py
@@ -0,0 +1,10 @@
+from Perceptron import Perceptron
+from PSO import PSO
+
+
+class NeuralNet:
+    def __init__(self, image, imageSize):
+        self.image = image
+        self.imageSize = imageSize
+
+    def run(self):
--- PSO.py
+++ PSO.py
@@ -2,6 +2,7 @@ import sys
 from Particle import Particle
 from Function import Function
 from Neighborhood import Neighborhood
+from NeuralNet import NeuralNet
 import statistics
 
 class PSO:
